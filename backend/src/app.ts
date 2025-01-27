9a98c77a9c57bed30731bbb663158c5ccd6f4760
Samson Daniel <samsoned@hotmail.com>
2025-01-27 15:30:27 +0100

    Update app to show basic response from backend.

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,6 +1,7 @@
 import express, { Request, Response } from 'express';
 import dotenv from 'dotenv';
 import routes from './routes';
+import cors from 'cors';
 
 dotenv.config();
 
@@ -8,6 +9,7 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 // Middleware
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
--- frontend/news-visor-frontend/src/app/app.component.ts
+++ frontend/news-visor-frontend/src/app/app.component.ts
@@ -1,12 +1,25 @@
+import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
   title = 'news-visor-frontend';
+  apiMessage: string = '';
+
+  constructor(private http: HttpClient) {}
+
+  ngOnInit(): void {
+    this.http.get<{ message: string }>('http://localhost:3000/api').subscribe({
+      next: (data) => {
+        this.apiMessage = data.message;
+      },
+      error: (err) => {
+        console.error('Error fetching API message', err);
+      }
+    });
+  }
 }
