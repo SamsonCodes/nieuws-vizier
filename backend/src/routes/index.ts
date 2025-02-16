6397c681aac7026eccff03968bdb95fbd0efe1da
Samson Daniel <samsoned@hotmail.com>
2025-02-16 11:48:54 +0100

    Improve error handling and logging.

--- backend/src/routes/index.ts
+++ backend/src/routes/index.ts
@@ -1,29 +1,29 @@
 import { Router, Request, Response, RequestHandler } from 'express';
 import dotenv from 'dotenv';
 dotenv.config();
-import axios from 'axios';
+import axios, { AxiosError } from 'axios';
 
 const WORLD_NEWS_API_URL = process.env.WORLD_NEWS_API_URL;
 const WORLD_NEWS_API_KEY = process.env.WORLD_NEWS_API_KEY;
 
 const router = Router();
 
-const newsHandler: RequestHandler = async (req: Request, res: Response) => {
+const newsHandler: RequestHandler = async (req: Request, res: Response): Promise<void> => {
   try {
-    let keyword = req.query.keyword;
-    console.log(keyword); //test
-    let config = {
+    const keyword = req.query.keyword;
+    console.log('Keyword: ' + keyword);
+    const config = {
       method: 'get',
       maxBodyLength: Infinity,
       url: `${WORLD_NEWS_API_URL}/search-news?source-country=nl&api-key=${WORLD_NEWS_API_KEY}&number=1&language=en&text=${keyword}`,
-      headers: { }
+      headers: {}
     };
-    
+
     const response = await axios.request(config);
 
-    // Map the news array to exclude the text field
+    // Map the news array to exclude the text field.
     const newsData = response.data.news.map((item: any) => {
-      const { text, ...rest } = item; // Destructure to exclude the text field. We don't want to include this in the response for legal reasons.
+      const { text, ...rest } = item;
       return rest;
     });
 
@@ -33,12 +33,18 @@ const newsHandler: RequestHandler = async (req: Request, res: Response) => {
       return;
     }
 
-    // Log the title of the first news item
     console.log(newsData[0].title);
-
-    // Send the modified response
     res.json({ ...response.data, news: newsData });
   } catch (error) {
+    if (axios.isAxiosError(error)) {
+      const axiosError = error as AxiosError;
+      const status = axiosError.response ? axiosError.response.status : 500;
+      const data = axiosError.response ? axiosError.response.data : 'No response data';
+      const message = (data as any).message;
+      console.error('Error:', message);
+      res.status(status).json({ error: message });
+      return;
+    }
     console.error(error);
     res.status(500).json({ error: 'An error occurred while fetching the news.' });
   }
--- frontend/src/app/components/home/home.component.ts
+++ frontend/src/app/components/home/home.component.ts
@@ -1,4 +1,4 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 
@@ -10,34 +10,38 @@ import { FormsModule } from '@angular/forms';
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit {
-  url: string = '';
-  title: string = '';
-  keyword: string = '';
-  noResultMessage: string = '';
+  url: string | undefined;
+  title: string | undefined;
+  keyword: string | undefined;
+  noResultMessage: string | undefined;
 
-  constructor(private http: HttpClient) {}
+  constructor(private http: HttpClient) { }
 
   ngOnInit(): void {
-    this.fetchNews();
+    // this.fetchNews();
   }
 
   fetchNews(): void {
+    console.log('Fetching news...');
     const params = this.keyword ? { params: { keyword: this.keyword } } : {};
     this.http.get<any>('http://localhost:3000/api', params).subscribe({
       next: (data) => {
         if (data.news && data.news.length > 0) {
-          this.noResultMessage = '';
+          this.noResultMessage = undefined;
           this.title = data.news[0].title;
           this.url = data.news[0].url;
         } else {
           this.noResultMessage = data.message || 'No news results found.';
-          this.title = '';
-          this.url = '';
+          this.title = undefined;
+          this.url = undefined;
         }
         console.log(data);
       },
-      error: (err) => {
-        console.error('Error fetching API message', err);
+      error: (err: HttpErrorResponse) => {
+        console.log(err.error.error);
+        this.noResultMessage = err.error.error || 'An error occurred while fetching the news.';
+        this.title = undefined;
+        this.url = undefined;
       }
     });
   }
