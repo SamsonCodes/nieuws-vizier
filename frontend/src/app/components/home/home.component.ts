1ad0791dd77111731b70d69407ed9d3ad264b14a
Samson Daniel <samsoned@hotmail.com>
2025-04-19 10:59:29 +0200

    Update frontend to show list of results.

--- frontend/src/app/components/home/home.component.ts
+++ frontend/src/app/components/home/home.component.ts
@@ -1,21 +1,21 @@
 import { HttpClient, HttpErrorResponse } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [FormsModule],
+  imports: [FormsModule, CommonModule],
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit {
-  url: string | undefined;
-  title: string | undefined;
+  news: { title: string; url: string; summary: string }[] = [];
   keyword: string | undefined;
   noResultMessage: string | undefined;
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient) {}
 
   ngOnInit(): void {
     // this.fetchNews();
@@ -28,20 +28,21 @@ export class HomeComponent implements OnInit {
       next: (data) => {
         if (data.news && data.news.length > 0) {
           this.noResultMessage = undefined;
-          this.title = data.news[0].title;
-          this.url = data.news[0].url;
+          this.news = data.news.map((item: any) => ({
+            title: item.title,
+            url: item.url,
+            summary: item.summary || 'No summary available'
+          }));
         } else {
           this.noResultMessage = data.message || 'No news results found.';
-          this.title = undefined;
-          this.url = undefined;
+          this.news = [];
         }
         console.log(data);
       },
       error: (err: HttpErrorResponse) => {
         console.log(err.error.error);
         this.noResultMessage = err.error.error || 'An error occurred while fetching the news.';
-        this.title = undefined;
-        this.url = undefined;
+        this.news = [];
       }
     });
   }
